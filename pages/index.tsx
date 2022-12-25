e3fbb9febcc459b736987dd07c55191252122f40
Yunus Emre GÜRLÜ <100469577+yunusemrgrl@users.noreply.github.com>
2022-12-25 18:06:31 +0300

    add validation access token and rememberMe on main page index.tsx and route.push correct page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,46 @@
 import type {NextPage} from 'next'
-import type {RootState} from '../store'
-import {useSelector, useDispatch} from 'react-redux'
-import {getToken} from "../slices/registerSlice";
-import {AppDispatch} from "../store";
+
 import Register from "./Register";
 import Login from "./Login";
+import {useEffect, useState,} from "react";
+import {TokenSlice,} from "../slices/authSlice";
+import {useDispatch, useSelector} from "react-redux";
+import {AppDispatch} from "../store";
+
+import {authSelect} from "../slices/authSlice"
+import {useRouter} from "next/router";
 
 const Home: NextPage = () => {
 
+    const [loading, setLoading] = useState(true);
 
     const dispatch = useDispatch<AppDispatch>()
-    const pokemon = useSelector((state: RootState) => state.accessToken)
+    const accessToken = useSelector(authSelect);
+    const router = useRouter();
+
+    const [isRemember, setIsRemember] = useState<boolean>(false);
+
+    useEffect(() => {
+        dispatch(TokenSlice.actions.checkCookie());
+        setLoading(false);
+
+        const remember = localStorage.getItem("rememberMe");
+        if (remember) {
+            setIsRemember(JSON.parse(remember));
+        }
+        if (isRemember && accessToken) {
+            router.push("/products")
+        }
+
+    }, [isRemember, accessToken])
 
 
-    return (
-        <>
-            <Register/>
-            <Login/>
-        </>
-    )
+    if (loading) {
+        return <div className="h-screen w-screen grid place-items-center">Yükleniyor...</div>;
+    }
+    if (accessToken) {
+        return (<Login/>)
+    }
+    return (<Register/>)
 }
 export default Home
